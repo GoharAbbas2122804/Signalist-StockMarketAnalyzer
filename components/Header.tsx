15205bf138a448492d95b9057a4fddfec00dbdc8
GoharAbbas2122804 <GoharAbbas2122804@gmail.com>
2025-10-10 15:19:02 +0500

    created a component named NavItems.tsx for navbar , that just displays on the md and large devices , next i will create the dropdown menu that will just display on the small devices for responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import NavItems from './NavItems'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header sticky top-0 '>
+      <div className='header-wrapper container'>
+        <Link href='/'>
+          <Image src='/assets/icons/logo.svg' alt='Signalist LOgo'  width={140} height={32} className='h-8 w-auto cursor-pointer' />
+        </Link>
+
+        <nav className='hidden sm:block '>
+          {/* NavItems component will be added here */}
+          <NavItems />
+        </nav>
+        {/* user drop down component will be added here */}
+      </div>
+
+    </header>
   )
 }
 
---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const NAV_ITEMS = [
+    {title: 'Dashboard', href: '/'},
+    {title: 'Search', href: '/search'},
+    {title: 'WatchList', href: '/watchlist'},
+    {title: 'About Devs', href: '/contact'},
+    {title: 'Contact', href: '/contact'},
+
+]
