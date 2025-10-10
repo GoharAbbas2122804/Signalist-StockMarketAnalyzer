d9a5a267c0e4522566bdae49300688d2c9a5808c
GoharAbbas2122804 <GoharAbbas2122804@gmail.com>
2025-10-10 14:07:53 +0500

    create the next js shadcn , initialized and installed the shadcn ui , create the root/layout.tsx to handle authorized and unauthorized access to dashboard and display header navbar component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex min-h-screen home-wrapper'>
+      HomePage
+    </div>
+  )
+}
+
+export default page
